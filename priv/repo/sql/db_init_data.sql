de97a9e703b54e0c35bb489cc173878d980a5ef0
seb3s <seb@matchix.com>
2020-03-22 18:42:24 +0100

    add init_data

---
+++ priv/repo/sql/db_init_data.sql
@@ -0,0 +1,5 @@
+insert into users (id, email, password_hash, firstname, lastname, language, active, role, inserted_at, updated_at) values
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
+    ***REMOVED***
