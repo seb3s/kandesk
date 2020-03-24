bac8158e28d03fdb904a75c49d53fc4b98f7edae
seb3s <seb@matchix.com>
2020-03-25 00:11:35 +0100

    add temp_id function

--- lib/kandesk/util.ex
+++ lib/kandesk/util.ex
@@ -3,4 +3,6 @@ defmodule Kandesk.Util do
   def to_integer(val) when is_integer(val), do: val
   def to_integer(val) when is_binary(val), do: String.to_integer(val)
 
+  def temp_id(), do: "auto" <> :erlang.integer_to_binary(rem :erlang.unique_integer(), 1000000)
+
 end
