0ad14dbad4d323e78f9658b2bc7fe73b5c5d843a
seb3s <seb@matchix.com>
2020-09-01 23:27:32 +0200

    remove db_init_data.sql that is replaced by seeds.exs

--- priv/repo/sql/db_init_data.sql
+++
@@ -1,2 +0,0 @@
-insert into users (id, email, password_hash, firstname, lastname, language, active, role, inserted_at, updated_at) values
-(default, 'user@user.com', '$argon2id$HASH', 'User', 'Lastname', 'fr', true, 'admin', now(), now());
