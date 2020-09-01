c6aaca06bc6864b2932349f5efa38cc7dac5493f
seb3s <seb@matchix.com>
2020-09-01 23:26:16 +0200

    add first admin user creation in seeds.exs to ease installation

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+Kandesk.Repo.insert!(%Kandesk.Schema.User{
+    email: "admin@admin.com",
+    password_hash: "$argon2id$v=19$m=131072,t=8,p=4$dpwcC7pR+VFv5UKNpd9bgQ$mc9rWpZqILOpxkEsAhKP0WG3kA6Jjn3C0kBWGU+ctuw",
+    firstname: "Firstname",
+    lastname: "Lastname",
+    language: "en",
+    role: "admin"
+    })
