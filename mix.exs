b1047f16e1517f38f9a53267901f897d9d8dfcd9
seb3s <seb@matchix.com>
2020-09-02 19:59:01 +0200

    LiveDashboard: enable "OS Data" & "Request Logger" tabs

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Kandesk.MixProject do
   def application do
     [
       mod: {Kandesk.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
