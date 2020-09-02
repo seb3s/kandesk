0ed5cb73396f19cef51463f3c19b89391dda049a
seb3s <seb@matchix.com>
2020-09-02 20:02:27 +0200

    add CheckRepoStatus

--- lib/kandesk_web/endpoint.ex
+++ lib/kandesk_web/endpoint.ex
@@ -33,6 +33,7 @@ defmodule KandeskWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
+    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :kandesk
   end
 
   plug Phoenix.LiveDashboard.RequestLogger,
