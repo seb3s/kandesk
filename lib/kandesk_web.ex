c7c494278c3ac13991ac7d24a408e26cd8530b35
seb3s <seb@matchix.com>
2020-12-03 13:49:52 +0100

    add subtemplate_view for use with liveviews

--- lib/kandesk_web.ex
+++ lib/kandesk_web.ex
@@ -41,6 +41,21 @@ defmodule KandeskWeb do
     end
   end
 
+  def subtemplate_view do
+    quote do
+      use Phoenix.View,
+        root: "lib/kandesk_web/templates",
+        namespace: KandeskWeb,
+        pattern: "**/*"
+
+      # Import convenience functions from controllers
+      import Phoenix.Controller, only: [get_flash: 1, get_flash: 2, view_module: 1]
+
+      # Include shared imports and aliases for views
+      unquote(view_helpers())
+    end
+  end
+
   def live_view do
     quote do
       use Phoenix.LiveView,
