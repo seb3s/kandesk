6b48984d1d28c8a5b2d449e746d99b675791e8c1
seb3s <seb@matchix.com>
2021-10-29 13:12:18 +0200

    add JS alias for later use

--- lib/kandesk_web.ex
+++ lib/kandesk_web.ex
@@ -106,6 +106,7 @@ defmodule KandeskWeb do
 
       # Import LiveView helpers (live_render, live_component, live_patch, etc)
       import Phoenix.LiveView.Helpers
+      alias Phoenix.LiveView.JS
 
       # Import basic rendering functionality (render, render_layout, etc)
       import Phoenix.View
